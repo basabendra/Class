bd8a7ed18109930b8b19cb96d9519ff37b390738
basabendra <basabendra@gmail.com>
2018-11-15 08:53:40 +0530

    Main activity & VCS XML committed

--- app/src/main/java/com/example/basabendrachattopadhyay/aclass/MainActivity.java
+++ app/src/main/java/com/example/basabendrachattopadhyay/aclass/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("myTag", "Message");
+        Log.i("myTag", "Message");
     }
 }
